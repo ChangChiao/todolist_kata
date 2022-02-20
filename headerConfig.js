944519b74ebff8995016310b7eaf5be23f904085
joe <joe.chang1014@gmail.com>
2022-02-20 17:09:46 +0800

    fix 中文亂碼

--- headerConfig.js
+++ headerConfig.js
@@ -3,5 +3,5 @@ module.exports = {
     "Content-Type, Authorization, Content-Length, X-Requested-With",
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "PATCH, POST, GET, OPTIONS, DELETE",
-  "Content-Type": "application/json",
+  "Content-Type": "application/json;charset=utf-8",
 };
